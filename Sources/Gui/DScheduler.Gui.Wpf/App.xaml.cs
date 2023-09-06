3b8f00460635d405847852969716aa04ef1f8678
Alex Kayeshko <alexey.kaeshko@altabel.com>
2023-09-06 15:42:26 +0300

    Add wpf gu project

---
+++ Sources/Gui/DScheduler.Gui.Wpf/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Prism.DryIoc;
+using System.Windows;
+
+using Prism.Ioc;
+using DryIoc;
+
+namespace DScheduler.Gui.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        /// <inheritdoc />
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+           
+        }
+
+        /// <inheritdoc />
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+
+        }
+    }
+}
